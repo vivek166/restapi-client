42eb991ab1c56b388253c72ec692f913cc9308b4
vivek166 <vivek.tiwari.cse16@gmail.com>
2017-05-29 19:29:21 +0530

    field validation

--- client/js/user/user.signin.js
+++ client/js/user/user.signin.js
@@ -1,12 +1,8 @@
 app.controller('userSigninCtrl', function($scope, $http, $location){
 	$scope.signinUser=function(){
-		/*$scope.userId=$scope.userId;
-		$scope.userPassword=$scope.userPassword;*/
-
 		var user={};
 		user.userId=$scope.userId;
 		user.userPassword=$scope.userPassword;
-
 		$http({
                 method: 'POST',
                 url: 'http://localhost:8080/projectmanagementapp/user/userAuth',
--- client/js/user/user.signup.js
+++ client/js/user/user.signup.js
@@ -1,16 +1,18 @@
 app.controller('userSignupCtrl', function($scope, $http, $location){
 	$scope.signupUser=function(){
-		var saveStatus = confirm('Are you sure you want to save');
-        if (saveStatus) {
         var user={};
 		user.userName=$scope.firstName+" "+$scope.lastName;
 		user.companyName=$scope.companyName;
 		user.userId=$scope.emailId;
-		/*user.password=$scope.password;
-		user.confirmPassword=$scope.confirmPassword;*/
-		user.userPassword=$scope.password+$scope.confirmPassword
-
-            $http({
+		if($scope.password==$scope.confirmPassword){
+			user.userPassword=$scope.password+$scope.confirmPassword;
+			save(user);
+		}else{
+			alert('user password must be match')
+		}
+	}
+	var save =function(user){
+		$http({
                 method: 'POST',
                 url: 'http://localhost:8080/projectmanagementapp/user',
                 data: user,
@@ -21,8 +23,5 @@ app.controller('userSignupCtrl', function($scope, $http, $location){
                 alert("record saved");
                 $location.path('/');
             })
-        } else {
-            alert('record not saved');
         }
-	}
 });
