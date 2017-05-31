dfe1b200158d1b81de74c750ac538df4d9b98a50
vivek166 <vivek.tiwari.cse16@gmail.com>
2017-05-31 08:49:00 -0400

    new field added in signup page

--- client/js/user/user.signup.js
+++ client/js/user/user.signup.js
@@ -1,20 +1,38 @@
-app.controller('userSignupCtrl', function($scope, $http, $location){
+app.controller('userSignupCtrl', ['$scope', function($scope, $http, $location){
+		$scope.skills = [];
+		$scope.addSkill = function() {
+			if( $scope.newSkill){
+					$scope.skills.push({'title': $scope.newSkill})
+					$scope.newSkill = ''
+			}
+		}
+
+		$scope.deleteSkill = function(index) {	
+			$scope.skills.splice(index, 1);
+		}
+
 	$scope.signupUser=function(){
         var user={};
-		user.userName=$scope.firstName+" "+$scope.lastName;
+        user.firstName=$scope.firstName;
+		user.lastName=$scope.lastName;
 		user.companyName=$scope.companyName;
-		user.userId=$scope.emailId;
+		user.email=$scope.email;
+		user.technologies=$scope.skills;
+		user.department=$scope.department;
+		user.type=$scope.type;
+		
 		if($scope.password==$scope.confirmPassword){
-			user.userPassword=$scope.password+$scope.confirmPassword;
+			user.password=$scope.password;
 			save(user);
 		}else{
 			alert('user password must be match')
 		}
+		console.log($scope.firstName+$scope.lastName+$scope.companyName+$scope.email+$scope.password+skills+$scope.department+$scope.type);
 	}
 	var save =function(user){
 		$http({
                 method: 'POST',
-                url: 'http://localhost:8080/projectmanagementapp/user',
+                url: 'http://localhost:8080/projectmanagementapp/employee',
                 data: user,
                 headers: {
                     'Content-Type': 'application/json'
@@ -24,4 +42,4 @@ app.controller('userSignupCtrl', function($scope, $http, $location){
                 $location.path('/');
             })
         }
-});
+}]);
