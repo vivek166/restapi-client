9ad1300bf4a8f8cd1764d7dda911e3eb30b8e359
vivek166 <vivek.tiwari.cse16@gmail.com>
2017-06-09 20:26:18 +0530

    dropdown value filled from database and project assined to employee

--- client/assign/assign.js
+++ client/assign/assign.js
@@ -1,39 +1,30 @@
 app.controller('assignCtrl', function($scope, $http, userInfo){
 	$scope.assign=function(){
-
-
-    getUsers();
-    getProjects();
-		/*var assign = {};
-		assign.employeeId=$scope.userId;
-		assign.projectId=$scope.projectId;
-
 		$http({
                 method: 'POST',
-                url: "http://localhost:8080/projectmanagementapp/user/"+$scope.employeeId+"/assignproject",
-                data: assign,
-                headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer '+userInfo.getUser().token
-                }
-            }).then(function(data, status, headers, config) {
-                alert("project assigned successefully");
-            });*/
-	}
-
-
-    var getProjects=function(){
-        $http({
-                method: 'GET',
-                url: "http://localhost:8080/projectmanagementapp/project",
-                data: assign,
+                url: "http://localhost:8080/projectmanagementapp/user/assignproject?userId="+$scope.userId+"&projectId="+$scope.projectId,
                 headers: {
                         'Content-Type': 'application/json',
                         'Authorization': 'Bearer '+userInfo.getUser().token
                 }
             }).then(function mySucces(response) {
-            $scope.projects = response.data.data;
-            console.log(response.data.data);
+            alert("project assigned successefully");
+        }, function myError(response) {
+           alert("project can not assigned");
+        });
+	   }
+
+
+    var getProjects=function(){
+        $http({
+                method: 'GET',
+                url: "http://localhost:8080/projectmanagementapp/project/filter",
+                headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer '+userInfo.getUser().token
+                }
+            }).then(function mySucces(response) {
+            $scope.projects = response.data;
         }, function myError(response) {
             $scope.myWelcome = response.statusText;
         });
@@ -41,19 +32,23 @@ app.controller('assignCtrl', function($scope, $http, userInfo){
     var getUsers=function(){
         $http({
                 method: 'GET',
-                url: "http://localhost:8080/projectmanagementapp/user",
-                data: assign,
+                url: "http://localhost:8080/projectmanagementapp/user/filter?query=employee",
                 headers: {
                         'Content-Type': 'application/json',
                         'Authorization': 'Bearer '+userInfo.getUser().token
                 }
             }).then(function mySucces(response) {
-            $scope.users = response.data.data;
-            console.log(response.data.data);
+                $scope.users=response.data;
         }, function myError(response) {
             $scope.myWelcome = response.statusText;
         });
     }
 
-   
+    $scope.refreshEmployeeId=function(){
+        getUsers();
+    }
+    $scope.refreshProjectId=function(){
+        getProjects();
+    }
+    
 });
